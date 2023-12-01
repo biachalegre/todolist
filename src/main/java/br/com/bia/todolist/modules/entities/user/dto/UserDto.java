11abca309966546797a18d821bf408d8aab943ec
Bia <95707803+biachalegre@users.noreply.github.com>
2023-12-01 19:42:05 -0300

    feat: criação dos atributos da entidade User

--- src/main/java/br/com/bia/todolist/modules/entities/user/dto/UserDto.java
+++ src/main/java/br/com/bia/todolist/modules/entities/user/dto/UserDto.java
@@ -1,5 +1,15 @@
 package br.com.bia.todolist.modules.entities.user.dto;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class UserDto {
-  
+  private UUID id;
+  private String name;
+  private String username;
+//  TODO: pesquisar como faz para criar uma senha que tenha caracteres especiais
+  private String password;
+  private LocalDateTime createdAt;
+
+
 }
